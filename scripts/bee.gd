6f9361a747c6d0384572c6dc07a7362f23460acf
OscarDebra <osc.debra@gmail.com>
2026-01-16 13:10:31 +0100

    removed gitignore on assets file

---
+++ scripts/bee.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+const HEX_DIRS = [
+	Vector2i(1, 0),
+	Vector2i(1, -1),
+	Vector2i(0, -1),
+	Vector2i(-1, 0),
+	Vector2i(-1, 1),
+	Vector2i(0, 1),
+]
+
+@onready var my_tilemap = get_parent().get_node("TileMapLayer")
+
+func _ready() -> void:
+	global_position = my_tilemap.map_to_local(HEX_DIRS[1])
+
+	print(TileMap)
+
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta) -> void:
+	pass
