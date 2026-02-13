fbc309e59bd25f45c3b977313d3eb68ea8b15f65
OscarDebra <osc.debra@gmail.com>
2026-02-13 14:22:16 +0100

    added a toolbar_slot scene and a toolbar scene

--- scripts/bee.gd
+++ scripts/bee.gd
@@ -1,36 +1,40 @@
 extends Node2D
-
 @onready var my_tilemap = get_parent().get_node("TileMapLayer")
-
-const path := [
-	Vector2i(1, 0),
-	Vector2i(1, 0),
-	Vector2i(0, -1),
-	Vector2i(-1, -1),
-	Vector2i(-1, 1),
-	Vector2i(0, 1),
-]
-
+var path : Array[Vector2i] = []  # Changed from const to var with type
+var move_tick := false
 var path_index := 0
 var map_position := Vector2i(0, 0)
-
-var is_moving := false
+var is_moving : bool = false
 var move_progress := 0.0
 var move_duration := 0.2
 var start_position := Vector2.ZERO
 var target_position := Vector2.ZERO
+var is_rotating : bool = false
+var rotation_progress := 0.0
+var rotation_duration := 0.2
+var start_rotation := 0.0
+var target_rotation := 0.0
 
 func _ready() -> void:
 	get_tree().get_root().get_node("main").global_tick.connect(_on_global_tick)
 	map_position = my_tilemap.local_to_map(global_position)
 
-
 func _process(delta) -> void:
-	if is_moving:
+	if is_rotating:
+		rotation_progress += delta / rotation_duration
+		
+		if rotation_progress >= 1.0:
+			rotation = target_rotation
+			start_rotation = target_rotation
+			is_rotating = false
+			rotation_progress = 0.0
+		else:
+			rotation = lerp_angle(start_rotation, target_rotation, rotation_progress)
+	
+	elif is_moving:  # Only move after rotation is complete
 		move_progress += delta / move_duration
 		
 		if move_progress >= 1.0:
-			# Snap to final position
 			global_position = target_position
 			start_position = target_position
 			is_moving = false
@@ -39,12 +43,29 @@ func _process(delta) -> void:
 			global_position = start_position.lerp(target_position, move_progress)
 
 func _on_global_tick():
-	if path_index == path.size():
-		path_index = 0
-	
-	map_position += path[path_index]
-	
-	start_position = global_position
-	target_position = my_tilemap.map_to_local(map_position)
-	is_moving = true
-	path_index += 1
+	if path.is_empty():
+		return  # Don't move if no path is set
+		
+	if move_tick == true:
+		# Start moving
+		if path_index == path.size():
+			path_index = 0
+		
+		map_position += path[path_index]
+		start_position = global_position
+		target_position = my_tilemap.map_to_local(map_position)
+		is_moving = true
+		path_index += 1
+		move_tick = false
+	else:
+		# Prepare rotation for the next move
+		var next_index = path_index % path.size()
+		var next_direction = path[next_index]
+		var future_position = my_tilemap.map_to_local(map_position + next_direction)
+		var dir = (future_position - global_position).normalized()
+		
+		start_rotation = rotation
+		target_rotation = dir.angle()
+		is_rotating = true
+		rotation_progress = 0.0
+		move_tick = true
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,24 +1,18 @@
 extends Node
-
 signal global_tick
-
 @onready var global_tick_timer: Timer = $GlobalTickTimer
 @onready var tilemap: TileMapLayer = $TileMapLayer
 @onready var line_2d: Line2D = $Line2D
 
 var placing_bee: bool = false
 var bee_path: Array[Vector2i] = []
+var bee_scene = preload("res://scenes/bee.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	global_tick_timer.start()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
 
 func _input(event):
 	if event.is_action_pressed("ui_fullscreen"):
@@ -26,50 +20,105 @@ func _input(event):
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-			
+	
 	if event.is_action_pressed("place_bee"):
-		placing_bee = true
-		
-	if event.is_action_pressed("left_click") and placing_bee == true:
+		if not placing_bee:
+			# Start placing
+			placing_bee = true
+			bee_path.clear()
+			line_2d.clear_points()
+		else:
+			confirm_bee_placement()
+	
+	if event.is_action_pressed("left_click") and placing_bee:
 		try_add_tile()
+	
+	if event.is_action_pressed("ui_cancel") and placing_bee:
+		# Cancel placement
+		cancel_bee_placement()
 
 func _on_tick_timer_timeout() -> void:
 	emit_signal("global_tick")
-	
 
-	
 func try_add_tile():
 	var tile = tilemap.local_to_map(tilemap.get_local_mouse_position())
 	
-	if bee_path.has(tile):
-		return
-
+	
 	if bee_path.is_empty():
 		bee_path.append(tile)
 		highlight(tile)
 		return
-
+	
 	var last := bee_path[-1]
 	if is_neighbor(last, tile):
 		bee_path.append(tile)
 		highlight(tile)
 
-
 func highlight(tile):
-	line_2d.add_point(tilemap.local_to_map(tile))
+	line_2d.add_point(tilemap.map_to_local(tile))
+
+func confirm_bee_placement():
+	if bee_path.size() < 3:
+		print("Path too short! Need at least 3 tiles for a cycle.")
+		cancel_bee_placement()
+		return
 	
+	if not is_neighbor(bee_path[-1], bee_path[0]):
+		print("Path must form a cycle! Last tile must be adjacent to first tile.")
+		cancel_bee_placement()
+		return
+	
+	# Convert absolute positions to relative path
+	var relative_path: Array[Vector2i] = []
+	for i in range(bee_path.size()):
+		var current = bee_path[i]
+		var next = bee_path[(i + 1) % bee_path.size()]
+		relative_path.append(next - current)
+	
+	# Spawn the bee
+	var bee = bee_scene.instantiate()
+	add_child(bee)
+	
+	bee.global_position = tilemap.map_to_local(bee_path[0])
+	bee.map_position = bee_path[0]
+	bee.path = relative_path
+	bee.path_index = 0
+	
+	placing_bee = false
+	bee_path.clear()
+	line_2d.clear_points()
+	
+	print("Bee placed with cycle path!")
+
+func cancel_bee_placement():
+	placing_bee = false
+	bee_path.clear()
+	line_2d.clear_points()
+	print("Bee placement cancelled.")
 
 func is_neighbor(a: Vector2i, b: Vector2i) -> bool:
-	var neighbor_offsets = [
-		Vector2i(1, 0), # Right
+	var neighbor_offsets_even = [
+		Vector2i(1, 0),   # Bottom Right
+		Vector2i(-1, 0),  # Bottom Left
 		Vector2i(1, -1), # Top Right
-		Vector2i(0, -1), # Top Left
-		Vector2i(-1, 0), # Left
-		Vector2i(-1, 1), # Bottom Left
-		Vector2i(0, 1) # Bottom Right
+		Vector2i(-1, -1),  # Top Left
+		Vector2i(0, -1),  # Up
+		Vector2i(0, 1)    # Down
 	]
-
-	for neighbor in neighbor_offsets:
-		if neighbor - a == b:
+	
+	var neighbor_offsets_odd = [
+		Vector2i(1, 1),  # Bottom Right
+		Vector2i(-1, 1),  # Bottom Left
+		Vector2i(1, 0),  # Top Right
+		Vector2i(-1, 0),  # Top Left
+		Vector2i(0, -1),   # Up
+		Vector2i(0, 1)    # Down
+	]
+	
+	# Determine which offset set to use based on the row
+	var offsets = neighbor_offsets_even if a.x % 2 == 0 else neighbor_offsets_odd
+	
+	for neighbor in offsets:
+		if a + neighbor == b:
 			return true
 	return false
